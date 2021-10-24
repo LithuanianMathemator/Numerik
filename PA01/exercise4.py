b6a07313018afe572f2d11f9890d5731e2cfeffa
Maxime Vorwerk <maxime@4-werk.com>
2021-10-25 01:50:20 +0200

    Aufgabe 4, noch nicht komplett getestet

---
+++ PA01/exercise4.py
@@ -0,0 +1,77 @@
+import utility as u
+import numpy as np
+
+import time
+
+def plot_bilateral():
+    # open images
+    B1_in = u.B1()
+    B2_in = u.B2()
+    C_in = u.C()
+    
+    _t_start = time.time()
+
+    # calculate bilateral filter
+    print('image: B1\n')
+    B1_out = bilateral(B1_in)
+    #print('\nimage: B2\n')
+    #B2_out = bilateral(B2_in)
+    #print('\nimage: C\n')
+    #C_out = bilateral(C_in)
+    print('')
+
+    _t_end = time.time()
+    print(_t_end - _t_start)
+
+    # display results
+    u.view(B1_in, B1_out)
+    #u.view(B2_out)
+    #u.view(C_out)
+
+    # wait for enter to terminate
+    input()
+
+# bilateral gaussian filter6
+def bilateral(image, s=1, rho_s=3, rho_r=75):
+    (m, n) = image.shape
+    target = np.ndarray(shape=(m, n), dtype=np.uint8)
+    
+    for i in range(m):
+        for j in range(n):
+            target[i, j] = calculate_pixel(image, i, j, m, n, s, rho_s, rho_r)
+            #u.progress(i, j, m, n)             # 80s
+            #print((i, j), end='\r')            # 38s
+                                                # 32s
+    return target
+
+# calculate pixel for bilateral gaussian filter
+def calculate_pixel(image, k, l, m, n, s, rho_s, rho_r):
+    sum = 0
+    for i in range(k-s, k+s+1):
+        for j in range(l-s, l+s+1):
+            dividend = w_s(k-i, l-j, rho_s) * w_r(int(image[k, l]) - int(image[c(i, j, m, n)]), rho_r)
+            divisor = 0
+            for u in range(k-s, k+s+1):
+                for v in range(l-s, l+s+1):
+                    divisor += w_s(k-u, l-v, rho_s) * w_r(int(image[k, l]) - int(image[c(u, v, m, n)]), rho_r)
+            value = ( dividend / divisor ) * int(image[c(i, j, m, n)])
+            sum += value
+    return sum
+
+# gaussian distance weight
+def w_s(x, y, rho_s):
+    exp_arg = -(x**2 + y**2) / (2*pow(rho_s,2))
+    return np.exp(exp_arg)
+
+# gaussian color weight
+def w_r(x, rho_r):
+    exp_arg = -pow(x,2) / (2*pow(rho_r,2))
+    return np.exp(exp_arg)
+
+# clamps values to dimensions -> mirrors at edges
+def c(x, y, m, n):
+    _x = -abs(abs(x) - (m-1)) + (m-1)
+    _y = -abs(abs(y) - (n-1)) + (n-1) 
+    return (_x, _y)
+
+plot_bilateral()
---
+++ PA01/utility.py
@@ -0,0 +1,65 @@
+from skimage import io
+import matplotlib.pyplot as plot
+
+_img_count = 0
+
+# open B1
+def B1():
+    return openPNG('B1.png')
+
+# open B2
+def B2():
+    return openPNG('B2.png')
+
+# open C
+def C():
+    return openPNG('C.png')
+
+# open PNG from path
+def openPNG(path):
+    try:
+        PNG = io.imread(path)
+    except:
+        try:
+            PNG = io.imread('.\PA01.\\' + path)
+        except:
+            print('Datei ' + path + ' nicht gefunden')
+            return
+    return PNG
+
+# display image
+def view(image1, image2, _cmap='gray'):
+    global _img_count
+    p = plot.figure(_img_count)
+    _img_count += 1
+
+    p.add_subplot(1, 2, 1)
+    plot.imshow(image1, cmap=_cmap)
+
+    p.add_subplot(1, 2, 2)
+    plot.imshow(image2, cmap=_cmap)
+
+    p = plot.show()
+
+# print progress (slow, x2)
+def progress(i, j, m, n):
+    pct_1 = i/(m-1)
+    pct_2 = j/(n-1)
+    _val1 = (pct_1 * 10)//1
+    _val2 = (pct_2 * 10)//1
+
+    print('\033[F', end='')
+    print('cols: [', end='')
+    for k in range(10):
+        if k <= _val1-1:
+            print('#', end='')
+        else:
+            print(' ', end='')
+    print('] ' + str(i+1) + '/' + str(m) + '   \nrows: [', end='')
+    for k in range(10):
+        if k <= _val2-1:
+            print('#', end='')
+        else:
+            print(' ', end='')
+    print('] ' + str(j+1) + '/' + str(n) + '   ', end='')
+        
