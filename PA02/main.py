bca9352e315de58dffa4e8aa92fb5f7dce70573d
Jonas Lorenz <86180214+LithuanianMathemator@users.noreply.github.com>
2021-11-21 21:23:13 +0100

    Update main.py

--- PA02/main.py
+++ PA02/main.py
@@ -189,3 +189,6 @@ if __name__ == "__main__":
 
     plt.tight_layout()
     plt.show()
+    
+    seamlessmatrix('water.jpg', 'bear.jpg', (18,18))
+    seamlessmatrix('bird.jpg', 'plane.jpg', (50,260))
